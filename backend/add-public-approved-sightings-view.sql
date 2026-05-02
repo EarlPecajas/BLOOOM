b72e009d37bd98b28469d5e8ab07cecb59b277c6
earlkirstein <earlwarren.pecajas@msugensan.edu.ph>
2026-05-02 13:48:11 +0800

    Recreate public approved sightings view to avoid column rename error

--- backend/add-public-approved-sightings-view.sql
+++ backend/add-public-approved-sightings-view.sql
@@ -6,7 +6,7 @@ SELECT
   s.scientific_name,
   s.common_names,
   s.identification_confidence,
-  s.observation_type,
+  DROP VIEW IF EXISTS public_approved_sightings;
   s.observation_date,
   to_char(s.observation_date, 'YYYY-MM') AS observation_month,
   s.mountain_name,
